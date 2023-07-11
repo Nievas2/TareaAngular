a543c204b7e81e436345825e3c6d1569d31e75c3
Nievas2 <angelgabrielnievas@gmail.com>
2023-07-11 13:05:25 -0300

    Avances en las tablas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Productos } from 'src/interface';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ejercicioAngular';
+  columnasTabla: string[] = ['nombre', 'costo'];
+  arreglo = [
+    {nombre:'Manzana', costos:'1200'},
+    {nombre:'Naranja', costos:'1000'},
+    {nombre:'Peras', costos:'800'},
+    {nombre:'Cerezas', costos:'1500'},
+    {nombre:'Uvas', costos:'1500'},
+    {nombre:'Sandia', costos:'2000'},
+    {nombre:'Melon', costos:'2500'},
+    {nombre:'Granada', costos:'500'},
+    {nombre:'Durazno', costos:'900'},
+];
+  Productos = 
+  [  
+    {nombre:'Monitor', costos:'10000'},
+    {nombre:'Mouse', costos:'8000'},
+    {nombre:'Teclado', costos:'9000'},
+    {nombre:'Mouse Pad', costos:'12000'},
+    {nombre:'Auriculares', costos:'20000'},
+    {nombre:'Gabinete', costos:'250000'},
+    {nombre:'Camara', costos:'15000'},
+    {nombre:'Microfono', costos:'15000'},
+    {nombre:'Cables', costos:'5000'},
+   
+];
 }
